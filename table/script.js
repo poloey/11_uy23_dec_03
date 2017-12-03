d1a06e0162e30c93aa26826d2c3104b22137f8f1
hello <hello@gmail.com>
2017-12-03 17:27:51 +0600

    changed in script file

--- table/script.js
+++ table/script.js
@@ -1,22 +1,35 @@
 function addPeople () {
 	// getting name value from input field
+	var name = document.getElementById('name').value;
 	// getting email value from input field
+	var email = document.getElementById('email').value;
 
 	// make textnode for name document.createTextNode
+	var nameNode = document.createTextNode(name);
 	// make a textnode for email document.createTextNode
-
+	var emailNode = document.createTextNode(email);
 
 	// create a td  for name field
+	var tdname = document.createElement('td');
 	// append name textNode to td
+	tdname.appendChild(nameNode);
 	// create a td  for email field
+	var tdEmail = document.createElement('td');
 	// append email textNode to td
+	tdEmail.appendChild(emailNode);
 
 	// create a tr element
+	var tr = document.createElement('tr');
 	// append name td to tr
+	tr.appendChild(tdname);
 	//append email td to tr
+	tr.appendChild(tdEmail);
 
 	// select table
+	var table = document.getElementById('table');
 	//append tr to table
-
+	table.appendChild(tr);
+	document.getElementById('name').value = '';
+	document.getElementById('email').value = '';
 
 }
