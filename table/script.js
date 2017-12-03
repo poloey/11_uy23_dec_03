9aba152f433fc313c8fc033c781df4bf879b26e7
hello <hello@gmail.com>
2017-12-03 17:13:00 +0600

    changed in script.js

---
+++ table/script.js
@@ -0,0 +1,22 @@
+function addPeople () {
+	// getting name value from input field
+	// getting email value from input field
+
+	// make textnode for name document.createTextNode
+	// make a textnode for email document.createTextNode
+
+
+	// create a td  for name field
+	// append name textNode to td
+	// create a td  for email field
+	// append email textNode to td
+
+	// create a tr element
+	// append name td to tr
+	//append email td to tr
+
+	// select table
+	//append tr to table
+
+
+}
